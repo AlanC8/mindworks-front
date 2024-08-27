3a4fa809f9caa828d49459f9013612a610ccbc40
AlanC8 <askvizi@bk.ru>
2024-08-27 17:13:01 +0500

    1.0.3: dynamic numbers

--- app/components/LastPage.tsx
+++ app/components/LastPage.tsx
@@ -1,19 +1,24 @@
-import { useRouter } from 'next/navigation';
-import React from 'react';
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const LastPage: React.FC = () => {
-    const router = useRouter()
+  const router = useRouter();
+  const numbers = [6, 7, 8, 9, 10];
+  const random = numbers[Math.floor(Math.random() * numbers.length)];
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-white to-purple-200 p-6">
       <div className="text-center">
         <p className="text-lg font-medium text-gray-800 mb-4">
           Ваша память была оценена на
         </p>
-        <p className="text-5xl font-bold text-gray-800 mb-6">6 из 10</p>
+        <p className="text-5xl font-bold text-gray-800 mb-6">{random} из 10</p>
         <p className="text-lg font-medium text-gray-800 mb-8">
           Перейдите к вашему личному плану обучения
         </p>
-        <button onClick={() => router.push("/trainings")} className="bg-purple-500 text-white py-3 px-6 rounded-full text-lg font-medium hover:bg-purple-600 transition">
+        <button
+          onClick={() => router.push("/trainings")}
+          className="bg-purple-500 text-white py-3 px-6 rounded-full text-lg font-medium hover:bg-purple-600 transition"
+        >
           Приступить к тренировкам!
         </button>
       </div>
